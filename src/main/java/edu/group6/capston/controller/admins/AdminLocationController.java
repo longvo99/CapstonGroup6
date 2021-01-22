a951f0bf902b1ba38a8de6de183383b65c098bd4
longnguyen040201 <46593195+longnguyen040201@users.noreply.github.com>
2021-01-22 10:54:01 +0700

    Update AdminLocationController.java

--- src/main/java/edu/group6/capston/controller/admins/AdminLocationController.java
+++ src/main/java/edu/group6/capston/controller/admins/AdminLocationController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("admin/locaiton")
+@RequestMapping("admin/location")
 public class AdminLocationController {
 	@RequestMapping(value ="/index")
 	public String Index() {
