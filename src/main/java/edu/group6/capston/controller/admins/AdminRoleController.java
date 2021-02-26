ff189218e9215328c2e4700cbb7d6bbf9760d335
nguyentien143 <nguyenvantien143@gmail.com>
2021-02-26 09:45:47 +0700

    Create AdminRoleController.java
    t

---
+++ src/main/java/edu/group6/capston/controller/admins/AdminRoleController.java
@@ -0,0 +1,92 @@
+package edu.group6.capston.controller.admins;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.group6.capston.models.LocationTypes;
+import edu.group6.capston.models.Roles;
+import edu.group6.capston.services.LocationTypeService;
+import edu.group6.capston.services.RoleService;
+import edu.group6.capston.utils.GlobalsConstant;
+
+@Controller
+@RequestMapping("admin/role")
+public class AdminRoleController {
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	@Autowired
+	private RoleService roleService;
+	
+	@RequestMapping(value ="/index")
+	public String Index(Model model) {
+		model.addAttribute("roleList" , roleService.findAll());
+		return "admin.role.index";
+	}
+	@RequestMapping(value ="/add")
+	public String Add(Model model) {
+		return "admin.role.add";
+	}
+	
+	@PostMapping(value ="/add")
+	public String Add(@Valid @ModelAttribute("roles") Roles roles, BindingResult br, RedirectAttributes rd,
+			HttpServletRequest request, Model model) {
+		if (br.hasErrors()) {
+			rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("error", null, Locale.getDefault()));
+			return "admin.role.add";
+		}
+		if (roleService.save(roles)) {
+			rd.addFlashAttribute(GlobalsConstant.MESSAGE,
+					messageSource.getMessage("success", null, Locale.getDefault()));
+			rd.addFlashAttribute("success", true);
+			return "redirect:/admin/role/index";
+		}
+		rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("error", null, Locale.getDefault()));
+		model.addAttribute("roles", roles);
+		return "admin.role.add";
+	}
+	
+	@PostMapping(value ="/edit")
+	public String Edit(@Valid @ModelAttribute("roles") Roles roles, BindingResult br, RedirectAttributes rd,
+			HttpServletRequest request, Model model) throws UnsupportedEncodingException {
+		if (br.hasErrors()) {
+			rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("error", null, Locale.getDefault()));
+			return "admin.role.index";
+		}
+		if (roleService.update(roles)) {
+			rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("success", null, Locale.getDefault()));
+			rd.addFlashAttribute("success", true);
+			return "redirect:/admin/role/index";
+		}
+		rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("error", null, Locale.getDefault()));
+		rd.addFlashAttribute("error", true);
+		return "redirect:/admin/role/index";
+	}
+	
+	@RequestMapping(value ="/del/{id}")
+	public String delete(@PathVariable Integer id, RedirectAttributes rd, HttpServletRequest request, Model mode) {
+		if (roleService.delete(id)) {
+			rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("error", null, Locale.getDefault()));
+			rd.addFlashAttribute("error", true);
+			return "redirect:/admin/role/index";
+		}
+		rd.addFlashAttribute(GlobalsConstant.MESSAGE, messageSource.getMessage("success", null, Locale.getDefault()));
+		rd.addFlashAttribute("success", true);
+		return "redirect:/admin/role/index";
+	}
+}
