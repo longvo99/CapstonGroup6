5701821003e894aaa2c12747cf87ba324fc8e9c1
longnguyen040201 <46593195+longnguyen040201@users.noreply.github.com>
2021-03-09 09:32:54 +0700

    src

--- src/main/java/edu/group6/capston/models/Product.java
+++ src/main/java/edu/group6/capston/models/Product.java
@@ -2,24 +2,29 @@ package edu.group6.capston.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-@Table(name = "Products")
+@Table(name = "Product")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Product {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
 	private int productId;
 	@Column
-	private String productName;
+	private String name;
 	@Column
 	private String price;
 	@Column
@@ -28,10 +33,9 @@ public class Product {
 	private String orderCount;
 	@Column
 	private String imagePath;
-	@Column
-	private int productCategoryId;
-	@Column
-	private int discountId;
+	@ManyToOne
+	@JoinColumn(name = "productCategoryId")
+	private ProductCategories productCategories;
 	@Column
 	private int locationId;
 	
