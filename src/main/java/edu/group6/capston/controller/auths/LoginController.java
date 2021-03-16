bde0fe936884f6d7c443171a696338496f7a3b4f
thanhhai98 <77822420+thanhhai98@users.noreply.github.com>
2021-03-16 10:39:54 +0700

    sadad

--- src/main/java/edu/group6/capston/controller/auths/LoginController.java
+++ src/main/java/edu/group6/capston/controller/auths/LoginController.java
@@ -1,14 +1,18 @@
 package edu.group6.capston.controller.auths;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("auth")
 public class LoginController {
-	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder; 
 	@RequestMapping(value = "/login")
 	public String LoginAdmin() {
+		System.out.println(bCryptPasswordEncoder.encode("123456"));
 		return "auths.admin.login";
 	}
 
